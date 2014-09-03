292fc17a42b13a608fda2eebe4bb249c6504a2c3
obray <obray@qmetric.co.uk>
2014-09-03 16:56:14 +0100

    Complete project copied from my work user

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ pe-game
 =======
 
 The Policy Expert game
+
+I'll have you know I made this 2 years before flappy bird became a thing.
---
+++ src/scores/scores.php
@@ -0,0 +1,33 @@
+<?php
+
+    if (!isset($_POST['name']) || !isset($_POST['score'])) {
+        return;
+    }
+
+    $name = $_POST['name'];
+    $score = $_POST['score'];
+
+    $file = file_get_contents('scores.json');
+    $json = json_decode($file);
+
+    if (!is_array($json)) {
+        $json = array();
+    }
+
+    array_push($json, array(
+        'name' => $name,
+        'score' => $score
+    ));
+
+    usort($json, )
+
+
+    $json.sort(function(a,b){return a.score - b.score});
+    $json.reverse();
+    $json.splice(25, 1);
+
+    var_dump($json);
+
+    file_put_contents('scores.json', json_encode($json));
+
+?>
